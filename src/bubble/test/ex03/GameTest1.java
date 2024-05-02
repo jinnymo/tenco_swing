8b62450aaa92564930216c443703625e049ce58a
ryeonng <zrkfudz29@naver.com>
2024-05-02 14:16:58 +0900

    .

---
+++ src/bubble/test/ex03/GameTest1.java
@@ -0,0 +1,7 @@
+package bubble.test.ex03;
+
+public class GameTest1 {
+	public static void main(String[] args) {
+		new BubleFrame();
+	}
+}
--- src/bubble/test/ex03/Player.java
+++ src/bubble/test/ex03/Player.java
@@ -1,4 +1,4 @@
-package bubble.test.ex01;
+package bubble.test.ex03;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
